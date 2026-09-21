8b7f1fe744616e5750efe9da31a6975880e69c97
manat414-dev-jp <manat414-dev-jp@users.noreply.github.com>
2026-09-21 18:01:02 +0900

    AndroidProducts: add COMMON_LUNCH_CHOICES for lunch menu

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_kltedcmactive.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_kltedcmactive-userdebug \
+    lineage_kltedcmactive-user \
+    lineage_kltedcmactive-eng
